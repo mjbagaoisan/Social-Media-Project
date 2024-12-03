2f54e650395d95afbfd336665168be07e85e6189
Grimreaper6661 <136058768+Grimreaper6661@users.noreply.github.com>
2024-12-03 00:28:17 -0800

    Update FriendGraph.java

--- src/main/FriendGraph.java
+++ src/main/FriendGraph.java
@@ -1,4 +1,15 @@
 package main;
+import java.util.*;
 
 public class FriendGraph {
+  private List<User>users;
+
+  public FriendGraph {
+  users = new ArrayList<>();
 }
+
+public void addUser(User user) {
+  users.add(user);
+}
+public void addFriendship(User user1, User user2) {
+  
