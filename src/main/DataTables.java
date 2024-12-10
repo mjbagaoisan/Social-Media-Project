f5a0886370d058b085781a42f81cc0aed1a91bf9
Marc Jowell Bagaoisan <marcjowellbagaoisan@gmail.com>
2024-12-10 14:56:32 -0800

    added packgage main for some files

--- src/main/DataTables.java
+++ src/main/DataTables.java
@@ -1,3 +1,6 @@
+
+package main;
+
 import dataStructures.HashTable;
 
 public class DataTables {
--- src/main/Interests.java
+++ src/main/Interests.java
@@ -1,3 +1,5 @@
+package main;
+
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
